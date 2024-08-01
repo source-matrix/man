7b55b71b5dfc680b31e3563826e66ac0c47e920f
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-08-01 03:55:49 +0300

    Update rename.py

--- phoenix/rename.py
+++ phoenix/rename.py
@@ -4,18 +4,18 @@ import phoenix.client
 import time
 
 client = phoenix.client.client
-@events.register(events.NewMessage(pattern=r".rename (.*)", outgoing=True))
+@events.register(events.NewMessage(pattern=r".تغيير الاسم (.*)", outgoing=True))
 async def rename(event):
-    ok = await event.edit("Nickname changing...")
+    ok = await event.edit("تغيير اللقب...")
     names = event.pattern_match.group(1).strip() 
     first_name = names
     last_name = ""
     if "/" in names:
         first_name, last_name = names.split("/", 1)
-        await event.edit('Nick changed succusfully')
+        await event.edit('تغير الاسم')
     try:
         await event.client(UpdateProfileRequest(first_name=first_name, last_name=last_name,)) 
-        await event.edit('Nick changed succusfully')
+        await event.edit('تغير الاسم بنجاح')
     except Exception as ex:
         await event.edit(ok, "\n`{}`".format(str(ex)))
     time.sleep(0.5)
