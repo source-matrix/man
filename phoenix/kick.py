8ac1f805c74e328a58d2756c6d74c316cc920102
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-08-01 03:36:07 +0300

    Update kick.py

--- phoenix/kick.py
+++ phoenix/kick.py
@@ -4,9 +4,9 @@ from telethon.tl.functions.users import GetFullUserRequest
 import phoenix.client
 client = phoenix.client.client
 
-@events.register(events.NewMessage(outgoing=True, pattern=r'\.kick'))
+@events.register(events.NewMessage(outgoing=True, pattern=r'\.طرد'))
 async def runkick(event):
-    await event.edit("Processing...")
+    await event.edit("جارٍ...")
     sleep(2)
     await event.delete()
     getmessage = await event.get_reply_message()
@@ -14,18 +14,18 @@ async def runkick(event):
     targetdetails = await client(GetFullUserRequest(targetuser))
     messagelocation = event.to_id
     getreason = event.message.raw_text.splitlines()
-    replacecmd = getreason[0].replace(".kick ", "")
+    replacecmd = getreason[0].replace(".طرد ", "")
     kickedreason = replacecmd.splitlines()
     reason = kickedreason[0]
     client.parse_mode = "html"
     try:
         await event.client.kick_participant(messagelocation, getmessage.sender.id)
         if reason:
-            if ".kick" in reason:
-                await event.client.send_message(messagelocation, f"<a href='tg://user?id={targetuser}'>{targetdetails.users[0].first_name}</a> kick qilindi")
+            if ".طرد" in reason:
+                await event.client.send_message(messagelocation, f"<a href='tg://user?id={targetuser}'>{targetdetails.users[0].first_name}</a> تم ركله")
             else:
-                await event.client.send_message(messagelocation, f"<a href='tg://user?id={targetuser}'>{targetdetails.users[0].first_name}</a> kick qilindi\nReason: {reason}")
+                await event.client.send_message(messagelocation, f"<a href='tg://user?id={targetuser}'>{targetdetails.users[0].first_name}</a> تم ركله\nسبب: {reason}")
         else:
-            await event.client.send_message(messagelocation, f"<a href='tg://user?id={targetuser}'>{targetdetails.users[0].first_name}</a> KICK QILINDI")
+            await event.client.send_message(messagelocation, f"<a href='tg://user?id={targetuser}'>{targetdetails.users[0].first_name}</a> تم تنفيذ الركلة")
     except:
         pass
