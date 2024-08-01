680d4bf3efd0c78c48676e69d3745f47f1fd9e43
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-08-01 03:39:57 +0300

    Update lovelyrun.py

--- phoenix/lovelyrun.py
+++ phoenix/lovelyrun.py
@@ -8,7 +8,7 @@ client = phoenix.client.client
 
 @events.register(events.NewMessage)
 async def lovelyrun(event):
-    if '.lovely' in event.raw_text:
+    if '.حب' in event.raw_text:
         time.sleep(0.3)
         for d in lovely.lovely:
             time.sleep(0.3)
