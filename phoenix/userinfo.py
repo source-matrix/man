6d4c265f288528c3358b8c1a0d9b378bd4be6bc6
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-08-01 13:27:01 +0300

    Update userinfo.py

--- phoenix/userinfo.py
+++ phoenix/userinfo.py
@@ -5,7 +5,7 @@ from os import remove
 
 client = phoenix.client.client
 
-@events.register(events.NewMessage(outgoing=True, pattern=r'\.منو'))
+@events.register(events.NewMessage(outgoing=True, pattern=r'\.ايدي'))
 async def userinfo(event):
     await event.delete()
     try:
