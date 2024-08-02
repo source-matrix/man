43096689ee299020ddbef9b084dd9fa490d6fd57
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-08-02 03:00:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import phoenix.client, phoenix.kick, phoenix.ketdim, phoenix.uzbrun, phoenix.whyrun, phoenix.iloveyou, phoenix.goodnight, phoenix.ahelp, phoenix.konspekt, phoenix.lovelyrun, phoenix.bombs, phoenix.help, phoenix.loading, phoenix.emoji, phoenix.dump, phoenix.sexy, phoenix.type, phoenix.magicrun, phoenix.animation, phoenix.animation2, phoenix.mute, phoenix.fuck, phoenix.rev, phoenix.tr, phoenix.userinfo, phoenix.base64, phoenix.react, phoenix.snow, phoenix.smsbomb, phoenix.rename, phoenix.iptrace, phoenix.spam, phoenix.alive, phoenix.tagall, phoenix.afk, phoenix.timer, phoenix.ping,phoenix.nashr
 import phoenix.allanimations as allanim 
 import os
+import Telegram.client
 #Developer: @I0I0II 
 
 #Modules
