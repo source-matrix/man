d694587cd52545c7d67d047830b19e3ae6aec888
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-08-03 16:07:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import phoenix.client, phoenix.kick, phoenix.ketdim, phoenix.uzbrun, phoenix.whyrun, phoenix.iloveyou, phoenix.goodnight, phoenix.ahelp, phoenix.konspekt, phoenix.lovelyrun, phoenix.bombs, phoenix.help, phoenix.loading, phoenix.emoji, phoenix.dump, phoenix.sexy, phoenix.type, phoenix.magicrun, phoenix.animation, phoenix.animation2, phoenix.mute, phoenix.fuck, phoenix.rev, phoenix.tr, phoenix.userinfo, phoenix.base64, phoenix.react, phoenix.snow, phoenix.smsbomb, phoenix.rename, phoenix.iptrace, phoenix.spam, phoenix.alive, phoenix.tagall, phoenix.afk, phoenix.timer, phoenix.ping
 import phoenix.allanimations as allanim 
 import os
+from phoenix.spam import repeat_message
 #Developer: @I0I0II 
 
 #Modules
@@ -108,6 +109,8 @@ client.add_event_handler(phoenix.kick.runkick)
 client.add_event_handler(phoenix.ketdim.ketdihandlers)
 client.add_event_handler(phoenix.uzbrun.uzbanim)
 client.add_event_handler(phoenix.whyrun.why)
+client.add_event_handler(repeat_message)
+
 	
 client.start()
 
