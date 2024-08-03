a16f224bd8aefdbd301441c7739d4c591ba9491d
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-08-03 04:12:24 +0300

    Update spam.py

--- phoenix/spam.py
+++ phoenix/spam.py
@@ -1,33 +1,34 @@
 from telethon import events
 import phoenix.client
 import asyncio
-import time
+client = phoenix.client.client
 
-client = phoenix.client.client  # تأكد من تهيئة العميل بشكل صحيح ببيانات تسجيل الدخول
+spamming = False
 
-async def get_group_ids():
-    dialogs = await client.get_dialogs()
-    group_ids = [dialog.entity.id for dialog in dialogs if dialog.is_group]
-    return group_ids
+@events.register(events.NewMessage(outgoing=True, pattern=".كرر ?(.*)"))
+async def delayspam(e):
+    global spamming
+    try:
+        args = e.text.split(" ", 3)
+        dark = float(args[1])
+        count = int(args[2])
+        msg = str(args[3])
+    except BaseException:
+        return await e.edit("**هكذا :** الامر <الوقت> <العدد> <الرسالة>")
+    await e.delete()
+    try:
+        spamming = True
+        for i in range(count):
+            if not spamming:
+                break
+            await e.respond(msg)
+            await asyncio.sleep(dark)
+        spamming = False
+    except Exception as u:
+        await e.respond(f"**خطا :** `{u}`")
 
-async def send_to_all_groups(message, delay, count):
-    group_ids = await get_group_ids()
-
-    for _ in range(count):
-        for group_id in group_ids:
-            await client.send_message(group_id, message)
-        await asyncio.sleep(delay)
-@client.on(events.NewMessage(pattern=r"\.نشر(\d+) -عدد (\d+)"))
-async def handler(event):
-    delay = int(event.pattern_match.group(1))
-    count = int(event.pattern_match.group(2))
-    await event.respond("بدء النشر...")
-    asyncio.create_task(send_to_all_groups(event.message.message, delay, count))
-
-@client.on(events.NewMessage(pattern=r"\.ايقاف_النشر"))
-async def stop_handler(event):
-    # هنا يمكنك إضافة منطق لإيقاف المهام المتزامنة
-    print("تم إيقاف النشر")
-
-client.start()
-client.run_until_disconnected()
+@events.register(events.NewMessage(outgoing=True, pattern=".ايقاف_التكرار"))
+async def stopspam(e):
+    global spamming
+    spamming = False
+    await e.respond("لقد توقف.")
