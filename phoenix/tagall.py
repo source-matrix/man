75ffc75aa7abb59c00faf36d754affa02b9442a2
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-09-16 18:19:01 +0300

    Update tagall.py

--- phoenix/tagall.py
+++ phoenix/tagall.py
@@ -3,15 +3,15 @@ import phoenix.client
 import time
 client = phoenix.client.client
 
-@events.register(events.NewMessage(pattern=".تاك"))
+@client.on(events.NewMessage(outgoing=True, pattern=".تاك_للكل"))
 async def tagall(event):
-		client.parse_mode = "html"
-		if event.fwd_from:
-			return
-		mentions = "<b>اعضاء المجموعة</b>\n"
-		chat = await event.get_input_chat()
-		async for x in client.iter_participants(chat, 100):
-			time.sleep(0.05)
-			mentions += f" \n [{x.first_name}] (<a href='tg://user?id={x.id}'>Profile</a>)"
-			time.sleep(0.05)
-			await event.edit(mentions)
+    if not isinstance(event, events.NewMessage.Event):
+        return
+    client.parse_mode = "html"
+    mentions = "<b>اعضاء المجموعة</b>\n"
+    chat = await event.get_input_chat()
+    async for x in client.iter_participants(chat, 100):
+        time.sleep(0.05)
+        mentions += f" \n [{x.first_name}] (<a href='tg://user?id={x.id}'>Profile</a>)"
+        time.sleep(0.05)
+    await event.edit(mentions)
