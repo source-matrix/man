417c9a0385560c99a52df0606194918af4eed12a
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-09-27 18:42:32 +0300

    Update fuck.py

--- FINAL/fuck.py
+++ FINAL/fuck.py
@@ -2,6 +2,6 @@ from telethon import events
 import FINAL.client
 client = FINAL.client.client
 
-@events.register(events.NewMessage(pattern=".احم"))
+@events.register(events.NewMessage(pattern=".تبا لك"))
 async def fuck(event):
 	await event.edit("┏━┳┳┳━┳┳┓\n┃━┫┃┃┏┫━┫┏┓\n┃┏┫┃┃┗┫┃┃┃┃\n┗┛┗━┻━┻┻┛┃┃\n┏┳┳━┳┳┳┓┏┫┣┳┓\n┣┓┃┃┃┃┣┫┃┏┻┻┫\n┃┃┃┃┃┃┃┃┣┻┫┃┃\n┗━┻━┻━┻┛┗━━━┛")
