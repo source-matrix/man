12ce185ad884c3f9f9aa5059374e1be00097cc46
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-09-29 17:44:58 +0300

    Update ahelp.py

--- FINAL/ahelp.py
+++ FINAL/ahelp.py
@@ -2,12 +2,12 @@ from telethon import events
 
 import FINAL.client
 client = FINAL.client.client
-@events.register(events.NewMessage(outgoing=True, pattern=".تسلية"))
+@events.register(events.NewMessage(outgoing=True, pattern=".م9"))
 async def ahelp(event):
 	client.parse_mode = "html"
 	await event.delete()
 	messagelocation = event.to_id
-	await event.client.send_message(messagelocation, ("""
+	await event.client.send_message(messagelocation, ("""**
 <b>قائمة التسلية</b>
 **ضع دوت (نقطة) قبل كل امر**
 
@@ -15,4 +15,4 @@ async def ahelp(event):
 
 وحش - خنزير  - قاتل - سلاح  -كلب - هلو همف - زوج- رشفة - مرحبا - ثعبان -قطة -همم - وداعا - شيتوز  -دسلايك - هينفو- النظام الشمسي - جيو - شرطة - طائرة - صندوق - مطر- احبك - فلايفمي - منصة - فراشة - قنبلة - احمق - ايموجي - احم - احبك2 - ذهبت - حب - سحر - تفاعل - ثلج
 <b>المطور:</b> @I0I0II 
-"""))
+**"""))
