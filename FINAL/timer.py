c2d9278511ef6e053a59cb83c41fddf4f926742f
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-09-30 17:36:22 +0300

    Update timer.py

--- FINAL/timer.py
+++ FINAL/timer.py
@@ -4,7 +4,8 @@ import asyncio
 from telethon import events
 import FINAL.client
 client = FINAL.client.client
-@events.register(events.NewMessage(outgoing=True, pattern=".مؤقت"))
+
+@events.register(events.NewMessage(outgoing=True, pattern="\.مؤقت"))
 async def timer(event):
         msg=event.message.raw_text.split()
         t=int(msg[1])
@@ -19,12 +20,12 @@ async def timer(event):
                 await event.edit(timer)
                 await asyncio.sleep(1)
                 t-=1
-        await event.edit("time out i!!!")
+        await event.edit("انتهى الوقت!!!")
         await asyncio.sleep(3)
         await event.delete()
 
 
-@events.register(events.NewMessage(outgoing=True, pattern=".numbers"))
+@events.register(events.NewMessage(outgoing=True, pattern="\.تنازلي"))
 async def numbers(event):
         msg=event.message.raw_text.split()
         t=int(msg[1])
@@ -34,33 +35,39 @@ async def numbers(event):
                 await client.send_message(event.message.to_id,str(t))
                 t-=1
 
-@events.register(events.NewMessage(outgoing=True, pattern=".setclock"))
+@events.register(events.NewMessage(outgoing=True, pattern="\.اسم"))
 async def setclock(event):
     from telethon.tl.functions.account import UpdateProfileRequest
-    await event.edit('ضبط الموقت . . .')
+    await event.edit('يتم تعيين الاسم الوقتي . . .')
     sleep(0.5)
     msg=event.message.raw_text.split()
     t=int(msg[1])
-    t*=60
+    t*=56
     while t>0:
             from datetime import datetime
-            today = datetime.today()
-            time= today.strftime("| %H:%M |")
+            now = datetime.now()
+            hour = now.hour
+            minute = now.minute
+            
+            hour = hour % 12
+            if hour == 0:
+                hour = 12  
+            time = f" {hour:02d}:{minute:02d} " 
             await client(UpdateProfileRequest(last_name=str(time)))
-            await event.edit('تم ضبط المؤقت')
+            await event.edit('تم تعيين الاسم الوقتي')
             sleep(0.5)
             await event.delete()
-            await asyncio.sleep(60)
+            await asyncio.sleep(56)
             t-=1
-    await client(UpdateProfileRequest(last_name="الوقت مهوم نسبيا !!"))
- 
+    await client(UpdateProfileRequest(last_name="الوقت تقريبي فقط !!"))
+
 
 
 
 from telethon import events
 from time import sleep
 
-@events.register(events.NewMessage(outgoing=True, pattern=r'\.finda'))
+@events.register(events.NewMessage(outgoing=True, pattern=r'\.المحذوفين'))
 async def runsda(event):
     await event.edit("البحث عن...")
     sleep(1)
@@ -74,17 +81,17 @@ async def runsda(event):
                 deletedid.append(users.id)
         countdeletedid = len(deletedid)
         if countdeletedid == 1:
-            await event.client.send_message(messagelocation, f"{countdeletedid} O'chirilgan hisoblar topildi \nGuruh nomi: {chatname}")
+            await event.client.send_message(messagelocation, f"{countdeletedid} حساب محذوف تم العثور عليه \nاسم المجموعة: {chatname}")
         elif countdeletedid == 0:
-            await event.client.send_message(messagelocation, f"O'chirilgan hizoblar topilmadi\nGuruh nomi: {chatname}")
+            await event.client.send_message(messagelocation, f"لم يتم العثور على أي حسابات محذوفة\nاسم المجموعة: {chatname}")
         else:
-            await event.client.send_message(messagelocation, f"{countdeletedid} O'chirilgan hisoblar topildi \nGuruh nomi: {chatname}")
+            await event.client.send_message(messagelocation, f"{countdeletedid} حساب محذوف تم العثور عليهم \nاسم المجموعة: {chatname}")
     except:
-        await event.client.send_message(messagelocation, "Something Went Wrong")
+        await event.client.send_message(messagelocation, "حدث خطأ ما")
 
-@events.register(events.NewMessage(outgoing=True, pattern=r'\.removeakk'))
+@events.register(events.NewMessage(outgoing=True, pattern=r'\.اطرد المحذوفين'))
 async def runrda(event):
-    await event.edit("waiting...")
+    await event.edit("الرجاء الانتظار...")
     sleep(1)
     await event.delete()
     messagelocation = event.to_id
@@ -97,29 +104,54 @@ async def runrda(event):
                 await event.client.kick_participant(messagelocation, users.id)
         countdeletedid = len(deletedid)
         if countdeletedid == 1:
-            await event.client.send_message(messagelocation, f"{countdeletedid} O'chirilgan hisoblar guruhdan o'chirib tashlandi\n guruh nomi:  {chatname}")
+            await event.client.send_message(messagelocation, f"{countdeletedid} حساب محذوف تم حذفه من المجموعة\n اسم المجموعة:  {chatname}")
         elif countdeletedid == 0:
-            await event.client.send_message(messagelocation, f"O'chirilgan hizoblar topilmadi\nGuruh nomi: {chatname}")
+            await event.client.send_message(messagelocation, f"لم يتم العثور على أي حسابات محذوفة\nاسم المجموعة: {chatname}")
         else:
-         await event.client.send_message(messagelocation, f"{countdeletedid} O'chirilgan hisoblar chiqarib tashlandi' {chatname}")
+         await event.client.send_message(messagelocation, f"{countdeletedid} حساب محذوف تم حذفهم من المجموعة {chatname}")
     except:
-        await event.client.send_message(messagelocation, "Some eror")
+        await event.client.send_message(messagelocation, "حدث خطأ ما")
  
+
 
 from os import remove
 
-@events.register(events.NewMessage(outgoing=True, pattern=r'\.واو'))
+auto_save_enabled = False
+
+@events.register(events.NewMessage(outgoing=True, pattern=r'\.واو|\.حفظ الذاتية'))
 async def rundrc(event):
     await event.delete()
-    try:
-        getrestrictedcontent = await event.get_reply_message()
-        downloadrestrictedcontent = await getrestrictedcontent.download_media()
-        await event.client.send_file("me", downloadrestrictedcontent)
-        remove(downloadrestrictedcontent)
-    except:
-        pass
+    if event.pattern_match.group(0) == ".واو":
+        try:
+            getrestrictedcontent = await event.get_reply_message()
+            downloadrestrictedcontent = await getrestrictedcontent.download_media()
+            await event.client.send_file("me", downloadrestrictedcontent)
+            remove(downloadrestrictedcontent)
+        except:
+            pass
+    elif event.pattern_match.group(0) == ".حفظ الذاتية":
+        global auto_save_enabled
+        auto_save_enabled = not auto_save_enabled
+        if auto_save_enabled:
+            await event.edit("تم تفعيل حفظ الوسائط ذاتية التدمير تلقائيًا.")
+        else:
+            await event.edit("تم إيقاف حفظ الوسائط ذاتية التدمير تلقائيًا.")
 
-@events.register(events.NewMessage(outgoing=True, pattern=r'\.msave'))
+@events.register(events.NewMessage)
+async def auto_save_media(event):
+    if auto_save_enabled:
+        try:
+            if event.media and event.media.ttl_seconds:  
+                downloadrestrictedcontent = await event.download_media()
+                await event.client.send_file("me", downloadrestrictedcontent)
+                remove(downloadrestrictedcontent)
+        except:
+            pass
+
+
+
+
+@events.register(events.NewMessage(outgoing=True, pattern=r'\.حفظ'))
 async def runrts(event):
     await event.delete()
     try:
@@ -129,9 +161,9 @@ async def runrts(event):
     except:
         pass
 
-@events.register(events.NewMessage(outgoing=True, pattern=r'\.rgm'))
+@events.register(events.NewMessage(outgoing=True, pattern=r'\.احداث'))
 async def runrgm(event):
-    await event.edit("recovery...")
+    await event.edit("جار التحويل اخر 10 احداث للرسائل المحفوظة...")
     sleep(1)
     await event.delete()
     try:
@@ -143,23 +175,28 @@ async def runrgm(event):
     except:
         pass
         
-@events.register(events.NewMessage(outgoing=True, pattern=".setbioclock"))
+@events.register(events.NewMessage(outgoing=True, pattern="\.بايو"))
 async def setbioclock(event):
-        from telethon.tl.functions.account import UpdateProfileRequest
-        await event.edit('Clock setting . . .')
+    from telethon.tl.functions.account import UpdateProfileRequest
+    await event.edit('لحظة من فضلك . . .')
+    sleep(0.5)
+    msg=event.message.raw_text.split()
+    t=int(msg[1])
+    t*=60
+    while t>0:
+        from datetime import datetime
+        now = datetime.now()
+        hour = now.hour
+        minute = now.minute
+       
+        hour = hour % 12
+        if hour == 0:
+            hour = 12  
+        time = now.strftime(f"{hour:02d}:{minute:02d} | %A | %d | %B | %Y")  
+        await client(UpdateProfileRequest(about=str(time)))
+        await event.edit('تم وضع البايو الوقتي')
         sleep(0.5)
-        msg=event.message.raw_text.split()
-        t=int(msg[1])
-        t*=60
-        while t>0:
-                from datetime import datetime
-                today = datetime.today()
-                time= today.strftime("%H:%M | %A | %d | %B | %Y")
-                await client(UpdateProfileRequest(about=str(time)))
-                await event.edit('Clock setted succesfully')
-                sleep(0.5)
-                await event.delete()
-                await asyncio.sleep(60)
-                t-=1 
-        await client(UpdateProfileRequest("Vaqt nisbiy tushuncha !!"))
- 
+        await event.delete()
+        await asyncio.sleep(60)
+        t-=1
+    await client(UpdateProfileRequest("Vaqt nisbiy tushuncha !!"))
