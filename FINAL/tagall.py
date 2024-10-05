e019485d4d2a93b6b874386f42d2648858aca4fd
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-10-05 14:57:00 +0300

    Update tagall.py

--- FINAL/tagall.py
+++ FINAL/tagall.py
@@ -18,12 +18,13 @@ async def tagall(event):
     permissions = await client.get_permissions(chat, me)
 
     if not permissions.is_admin:
-        await event.edit("عذرًا، لا أملك صلاحية ذكر الجميع في هذه المجموعة.")
+        await event.respond("عذرًا، لا أملك صلاحية ذكر الجميع في هذه المجموعة.")
         return
 
     all_participants = await client.get_participants(chat)
     total_members = len(all_participants)
     hidden_members_found = False
+
     all_mentions = []
     for user in all_participants:
         if user.deleted:
@@ -35,15 +36,16 @@ async def tagall(event):
         except ValueError:
             continue
         all_mentions.append(f"<a href='tg://user?id={user.id}'>{user.first_name}</a>")
+
     mentions_lists = [all_mentions[i:i+100] for i in range(0, total_members, 100)]
+
     final_mentions = ""
     for mentions_list in mentions_lists:
         if message_text:
             final_mentions += f"<b>{message_text}</b>\n"
-        final_mentions += " ".join(mentions_list) + "\n\n" 
+        final_mentions += " ".join(mentions_list) + "\n\n"  
 
     if hidden_members_found:
         final_mentions += "(لا يمكن ذكر الأعضاء المخفيين)\n"
 
-    # الساعة ب ٤:٢٠ تعبتتتتتتت
-    await event.edit(final_mentions)
+    await client.send_message(chat, final_mentions, parse_mode="html") 
