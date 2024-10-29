f45d8b0dfa0e4695826775b7ba441940fcbfe5ab
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-10-29 17:44:09 +0300

    Delete os.py

--- os.py
+++
@@ -1,5 +0,0 @@
-import os, sys
-os.system("pip install --upgrade pip")
-os.system("pkg install git")
-os.system("git clone --depth 1 https://github.com/1mrxe1/FINALv4 && cd FINALv4")
-os.system("python3 main.py") 
